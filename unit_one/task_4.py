fcf9fc9c63a6da291cea5bd173df5ee450b2570a
Kathbrandt <kathbrandt@yandex.ru>
2022-09-18 18:18:37 +0300

    добавлено домашнее задание

---
+++ unit_one/task_4.py
@@ -0,0 +1,7 @@
+# todo: Данные две переменные:
+# Нужно обменять значения переменных местами. В итого age должен равнятся 25 а  temperature – 36.6:
+age = 36.6
+temperature = 25
+new_age = temperature
+new_temp = age
+print("age =", new_age, ";",  "temperature =", new_temp)
